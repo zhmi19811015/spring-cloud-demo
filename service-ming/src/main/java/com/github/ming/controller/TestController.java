0e09a339f1add24437695cbc50f566d72a9dbae9
zhangiming <93691524@qq.com>
2019-03-08 15:55:54 +0800

    增加nacos

---
+++ service-ming/src/main/java/com/github/ming/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.github.ming.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class TestController {
+
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public TestController(RestTemplate restTemplate) {this.restTemplate = restTemplate;}
+
+    @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
+    public String echo(@PathVariable String str) {
+        return restTemplate.getForObject("http://nacos-server/config/echo/" + str, String.class);
+    }
+
+}
