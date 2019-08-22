c697e6c0f50069ae69bd1664d7911827c0eb7dd0
zhangm <zhangm@ahtsoft.com>
2019-08-22 17:09:48 +0800

    修改相关

---
+++ server-zipkin/src/main/java/com/github/ming/serverzipkin/ServerZipkinApplication.java
@@ -0,0 +1,17 @@
+package com.github.ming.serverzipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin2.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZipkinServer
+public class ServerZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerZipkinApplication.class, args);
+	}
+
+}
